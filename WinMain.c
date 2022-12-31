86513b75eee3b5c539b23ad3493719a744811c82
Hyun Jae Cho <50690938+chohyunjae163@users.noreply.github.com>
2022-12-31 12:39:55 +0900

    Update WinMain.c

--- WinMain.c
+++ WinMain.c
@@ -2,20 +2,68 @@
 #define internal static
 #define local_persist static
 #define global_variable static
+
 global_variable int Running;
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void
+Win32ResizeDIBSection(int Width, int Height)
+{
+    
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    
+    if(BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+    }
+    
+    if(!BitmapDeviceContext)
+    {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+    BitmapHandle = CreateDIBSection(BitmapDeviceContext,
+                                    &BitmapInfo,  
+                                    DIB_RGB_COLORS,       
+                                    &BitmapMemory, 
+                                    0, 0);
+}
+
+internal void
+Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+    StretchDIBits(DeviceContext, 
+                  X, Y, Width, Height,
+                  X, Y, Width, Height,
+                  BitmapMemory, 
+                  &BitmapInfo,
+                  DIB_RGB_COLORS, SRCCOPY);   
+}
+
 
 LRESULT CALLBACK
-MainWindowCallback(HWND Window, 
-                   UINT Message,
-                   WPARAM WParam,
-                   LPARAM LParam)
+Win32MainWindowCallback(HWND Window, 
+                        UINT Message,
+                        WPARAM WParam,
+                        LPARAM LParam)
 {
     LRESULT Result = 0;
     switch (Message)
     {
         case WM_SIZE:
         {
-            OutputDebugStringA("WM_SIZE\n");
+            RECT ClientRect;
+            GetClientRect(Window,&ClientRect);
+            int Width = ClientRect.right - ClientRect.left;
+            int Height = ClientRect.bottom - ClientRect.top;
+            Win32ResizeDIBSection(Width, Height);
         } break;
         
         case WM_DESTROY:
@@ -43,17 +91,7 @@ MainWindowCallback(HWND Window,
             int Y = Paint.rcPaint.top;
             int Width = Paint.rcPaint.right - Paint.rcPaint.left;
             int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-            local_persist DWORD Operation = WHITENESS;
-            if(Operation == WHITENESS)
-            {
-                Operation = BLACKNESS;
-            }
-            else
-            {
-                Operation = WHITENESS;
-            }
-            PatBlt(DeviceContext,X,Y,Width,Height,Operation);
-            
+            Win32UpdateWindow(DeviceContext,X,Y,Width,Height);
             EndPaint(Window,&Paint);
         }break;
         default:
@@ -75,7 +113,7 @@ WinMain(
 {
     WNDCLASS WindowClass = {0};
     WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.lpfnWndProc = Win32MainWindowCallback;
     WindowClass.hInstance = Instance;
     //WindowClass.hIcon;
     WindowClass.lpszClassName = "DungeonHeroClass";
